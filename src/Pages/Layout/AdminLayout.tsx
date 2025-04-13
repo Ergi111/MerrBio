cab0548651cd2f1f138d22b3f11bac311aab4874
BjoniMiraka <bjoni.miraka@fshnstudent.info>
2025-04-13 12:11:29 +0200

    improv ui

--- src/Pages/Layout/AdminLayout.tsx
+++ src/Pages/Layout/AdminLayout.tsx
@@ -1,3 +1,4 @@
+import { useEffect } from "react";
 import { Navbar } from "../../components/NavBar";
 import {
   Table,
@@ -8,20 +9,31 @@ import {
   TableRow,
 } from "../../components/ui/table";
 import { useLanguage } from "../../context/LanguageContext";
+import { useAuth } from "../../context/useAuth";
+import { getRoleFlags } from "../../utils/getRoleFlags";
+import { routerPaths } from "../../constants/routerPaths";
+import { useNavigate } from "react-router";
 import { useGetUsers } from "../../services/useGetUsers";
 
 export const AdminLayout = () => {
   const { t } = useLanguage();
-  const currentUserId = "someUserId";
-  const { users } = useGetUsers(currentUserId);
+  const navigate = useNavigate();
+  const { currentUser } = useAuth();
+  const { isAdmin } = getRoleFlags(currentUser?.role);
+  useEffect(() => {
+    if (!isAdmin) {
+      navigate(routerPaths.default);
+    }
+  }, [currentUser, isAdmin, navigate]);
+  const { users } = useGetUsers(currentUser?.id);
   return (
     <div className="min-h-screen flex flex-col">
       <Navbar />
       <div className="flex-grow bg-gray-50 p-10">
-        <Table>
+        <Table className="border rounded-lg">
           <TableHeader>
             <TableRow>
-              <TableHead>{t("name")}</TableHead>
+              <TableHead>{t("Name")}</TableHead>
               <TableHead>{t("email")}</TableHead>
               <TableHead>{t("role")}</TableHead>
             </TableRow>
